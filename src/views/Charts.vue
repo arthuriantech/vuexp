fcb988c36371fdef48a4fcf054726ab5ca822b65
Your Name <af3.inet@gmail.com>
2019-05-16 04:17:43 +0300

    Trololo: chartsjs

--- src/views/Charts.vue
+++ src/views/Charts.vue
@@ -10,7 +10,7 @@
       </div>
       <div class="chart">
         Bar
-        <bar-chart :chart-data="chartLine" :options="options"></bar-chart>
+        <bar-chart :chart-data="chartBar" :options="options"></bar-chart>
       </div>
       <div class="chart">
         Average cost
@@ -24,6 +24,7 @@
 <script>
 import axios from 'axios'
 
+// Диапазон
 function randrange(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -36,8 +37,12 @@ export default {
   },
   data () {
     return {
-      options: {maintainAspectRatio: false},
-      chartLine: null,
+      options: {maintainAspectRatio: false}, // отключить автоматическое соотношение сторон
+      // первые два графика совместимы между собой по формату данных
+      chartLine: {},
+      chartBar: {},
+
+      // третий график (круг) требует преобразования
       chartPie: {}
     }
   },
@@ -53,22 +58,32 @@ export default {
         let pieData = []
         let pieColors = []
 
+        // i - объект валюты
         for (let i of response.data.datasets) {
+          // интерполированная строка со случаный цветом для каждой валюты
+          // например rgba(150, 120, 200, 0.5), 0.5 - полупрозрачность
           let color = `rgba(${randrange(0, 200)}, ${randrange(0, 200)}, ${randrange(0, 200)}, 0.5)`
+
+          // добавление объект dataset
+          this.chartLine.datasets.push({backgroundColor: color, fill: true, ...i})
+
+          // средняя стоимость (average) валюты для Pie (круговой диаграммы)
           let avg = i.data.reduce((a, b) => a+b, 0) / i.data.length
 
           pieLabels.push(i.label)
           pieData.push(avg)
           pieColors.push(color)
-          this.chartLine.datasets.push({backgroundColor: color, fill: true, ...i})
         }
-        console.log(pieLabels)
+
+        this.chartBar = this.chartLine // они совместимы
+
+        // круговая диагармма
         this.chartPie = {
-          labels: pieLabels,
-          datasets: [
+          labels: pieLabels, // вместо годов должны быть названия валют
+          datasets: [ // вместо множества - всегда один элемент
           {
-            backgroundColor: pieColors,
-            data: pieData
+            backgroundColor: pieColors, // цвета для каждой валюты
+            data: pieData // средняя стоимость каждой валюты
           }
           ]
         }
@@ -77,6 +92,7 @@ export default {
   },
   watch: {
     '$route': {
+      // перерисовка графиков при заходе на страницу
       handler () {
         this.render()
       },
